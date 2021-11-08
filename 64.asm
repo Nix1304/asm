1236db42c3726709cf9882af3ddda981cceaafd8
Nix13 <nix1304@gmail.com>
2021-11-09 00:54:51 +0300

    more comments

--- 64.asm
+++ 64.asm
@@ -1,21 +1,20 @@
 global _main
 
-
 section .text
+_main: ; entry point for programm
+    call print
 
-_main:
-    mov     rax, 0x2000004 ; write
-    mov     rdi, 1 ; stdout
-    mov     rsi, msg
-    mov     rdx, msg.len
-    syscall
+print:
+    mov     rax, 0x2000004 ; sytem call for write
+    mov     rdi, 1 ; file handle 1 is stdout
+    mov     rsi, msg ; addres of string to output
+    mov     rdx, len ; number of bytes
+    syscall ; invoke operating system to do the write
 
-    mov     rax, 0x2000001 ; exit
-    mov     rdi, 0
-    syscall
-
+    mov     rax, 0x2000001 ; system call for exit
+    mov     rdi, 0 ; exit code 0
+    syscall ; invoke operating system to exit
 
 section .data
-
-msg: db      "Hello, world!", 10
-.len:   equ     $-msg
+msg: db "Hello, world!", 10 ; allocate space for string
+len: equ $-msg ; const
--- buid.sh
+++ buid.sh
@@ -1,2 +1,2 @@
-nasm -f macho64 64.asm
-ld -macosx_version_min 11.0 -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -lSystem -o 64 64.o
+nasm -f macho64 -o build/64.o 64.asm
+ld -macosx_version_min 11.0 -L /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -lSystem -o build/64 -no_pie build/64.o
